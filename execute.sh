29fcf0f0a32f6ecbe280271136a6548963861d8f
Alberto Berral <31618843+aberral@users.noreply.github.com>
2019-06-26 12:42:25 +0200

    Delete execute.sh

--- execute.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-##
-# Main of backup.sh
-# Script created on 02-04-19
-TIMESTAMP=`date "+%d_%m_%Y--%H_%M_%S"`
-# Creamos el log de la copia
-/scripts/./backup.sh 2>&1 | tee /home/aberral/.duplicity/logs/$TIMESTAMP
